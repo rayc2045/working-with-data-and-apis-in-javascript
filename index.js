7eed3238cd289d5186712bd930fbc4ce669ca300
Ray C <rayc2045@gmail.com>
2025-12-04 11:06:36 +0800

    Add "2.3 HTTP Post Request with fetch().html" and add the file write function to index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express(),
   port = 3000;
 
 app.use(express.static(__dirname));
+app.use(express.json({ limit: "1mb" }));
 
 app.listen(port, () => {
-  console.log(`伺服器運行於 http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
+});
+
+app.post("/api", (req, res) => {
+  const { latitude, longitude } = req.body;
+  const filePath = "data/location.txt";
+  const contentToAppend = `${latitude}, ${longitude}\n`;
+
+  try {
+    fs.appendFileSync(filePath, contentToAppend);
+    console.log("Content successfully appended to the file synchronously!");
+    res.json({
+      status: "success",
+      latitude,
+      longitude,
+    });
+  } catch (err) {
+    console.error("Error appending to file synchronously:", err);
+  }
 });

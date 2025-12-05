88b73843e5df152262453030abe7057fbad4ec8d
Ray C <rayc2045@gmail.com>
2025-12-05 13:10:15 +0800

    Add "2.4 Saving to a Database.html"

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-const express = require("express");
-const fs = require("fs");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+// import fs from "fs";
+import fs from "fs/promises";
+import { JSONFilePreset } from "lowdb/node";
 
-const app = express(),
-  port = 3000;
+(async () => {
+  const app = express(),
+    port = 3000,
+    __filename = fileURLToPath(import.meta.url),
+    __dirname = path.dirname(__filename);
 
-app.use(express.static(__dirname));
-app.use(express.json({ limit: "1mb" }));
+  app.use(express.static(__dirname));
+  app.use(express.json({ limit: "1mb" }));
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+  });
 
-app.post("/api", (req, res) => {
-  const { latitude, longitude } = req.body;
-  const filePath = "data/location.txt";
-  const contentToAppend = `${latitude}, ${longitude}\n`;
+  await fs.mkdir(path.join(__dirname, "data"), { recursive: true });
+  const defaultData = { logs: [] },
+    db = await JSONFilePreset(
+      path.join(__dirname, "data/logs.json"),
+      defaultData,
+    );
 
-  try {
-    fs.appendFileSync(filePath, contentToAppend);
-    console.log("Content successfully appended to the file synchronously!");
-    res.json({
-      status: "success",
-      latitude,
-      longitude,
-    });
-  } catch (err) {
-    console.error("Error appending to file synchronously:", err);
-  }
-});
+  app.post("/api", async (req, res) => {
+    const { latitude, longitude } = req.body;
+    // const filePath = "data/location.txt";
+    // const contentToAppend = `${latitude}, ${longitude}\n`;
+    try {
+      // fs.appendFileSync(filePath, contentToAppend);
+      const timestamp = Date.now();
+      await db.update(({ logs }) =>
+        logs.push({
+          timestamp,
+          ...req.body,
+        }),
+      );
+      res.json({
+        status: "success",
+        timestamp,
+        latitude,
+        longitude,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  });
+})();
